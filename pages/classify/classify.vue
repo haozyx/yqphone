5eb1b7a0b1c98f048e5f1e0fee5bb772ae27df16
haozyx <826060882@qq.com>
2019-10-11 17:00:49 +0800

    xiangqing

--- pages/classify/classify.vue
+++ pages/classify/classify.vue
@@ -1,6 +1,31 @@
 <template>
-	<view>
-		hehe
+	<view class="page-fill">
+		<view class="head">
+			<view class="headwrap">
+				<image class="searchimg" src="../../static/img/search.png"></image>
+				 <input type="text" class="searchtext" confirm-type="search" 
+				 placeholder="搜索..."  maxlength="20" placeholder-class="textstyle"/>
+			</view>
+		</view>
+		<view class="body">
+			<view class="left">
+				<view class="leftTitle">
+					事业单位
+				</view>
+				<!-- :class="" -->
+				<block v-for="(er,index) in childarry" :key="index">
+					<view class="lefttext" 
+					 :class="[{'selectcolor':cursel== index},index%2==0?'color-white':'color-grey']"
+					 @tap="changeColor" :data-index="index">
+						{{er}}
+					</view>
+				 </block>
+			</view>
+			
+			<view class="right">
+				
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,11 +33,17 @@
 	export default {
 		data() {
 			return {
-				
+				cursel:0,
+				childarry:['全部','园林中心','电业局','公积金中心','档案局','审批中心']
 			}
 		},
 		methods: {
-			
+			changeColor(e){
+				var me = this;
+				var index = e.currentTarget.dataset.index;
+				console.info(index);
+				me.cursel = index;
+			}
 		}
 	}
 </script>
