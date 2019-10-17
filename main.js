f130f30b6a01a76723bb78278f6fcf77b77cee08
haozyx <826060882@qq.com>
2019-10-17 18:59:18 +0800

    增加启动页

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-Vue.prototype.websiteUrl = 'https://yohaoyun.com/ycxq/test/';  
+Vue.prototype.websiteUrl = 'https://yohaoyun.com/ycxq/m/';  
 App.mpType = 'app'
 
 const app = new Vue({
---
+++ pages/indeximg/indeximg.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="page">
+		<image  class="indeximg" src="../../static/img/yindao.png"></image>
+		<button type="default" class="button" @tap="toindex">点击进入</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			toindex(){
+				uni.navigateTo({
+					url:'/pages/index/index'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+@import url("indeximg.css");
+</style>
